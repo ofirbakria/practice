a4107ff7f3e771ffdf0dd8b073f9b7e3066f1e38
ofirbakria <151513741+ofirbakria@users.noreply.github.com>
2024-05-07 18:49:32 +0300

    Update oferpractice.tf

--- terraform/oferpractice.tf
+++ terraform/oferpractice.tf
@@ -12,6 +12,7 @@ resource "aws_instance" "ofer_practice" {
   associate_public_ip_address = true
   vpc_security_group_ids      = ["sg-01dfcb524956905ff"]
 
+  user_data = file("user_data.sh")
   tags = {
     Name = "demo"
   }
